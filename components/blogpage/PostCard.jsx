fef508c09d2942f685a0ceef1fd825e86e8bffae
xebecs <shoeb.mo87@gmail.com>
2022-05-22 19:20:47 +0200

    deployment fix

--- components/blogpage/PostCard.jsx
+++ components/blogpage/PostCard.jsx
@@ -16,8 +16,8 @@ const PostCard = ({ post }) => {
       </h1>
       <div className='block lg:flex text-center items-center justify-center mb-8 w-full'>
         <div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8'>
-          <img src={post.author.photo?.url} alt={post.author.name} height='30px' width='30px' className='align-middle rounded-full'/>
-          <p className='inline align-middle text-Frog ml-2 text-lg'>{post.author.name}</p>
+          <img src={post.author?.photo?.url} alt={post.author?.name} height='30px' width='30px' className='align-middle rounded-full'/>
+          <p className='inline align-middle text-Frog ml-2 text-lg'>{post.author?.name}</p>
         </div>
         <div className='font-medium text-Frog'>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-Orchid" fill="none" viewBox="0 0 24 24" stroke="currentColor">
